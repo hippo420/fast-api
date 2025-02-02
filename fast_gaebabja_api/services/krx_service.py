f14384e0a7523262967c6089b4551706a68d3d14
gaebabja <hippo420@naver.com>
2025-02-02 20:44:17 +0900

    최초커밋

--- fast_gaebabja_api/services/krx_service.py
+++ fast_gaebabja_api/services/krx_service.py
@@ -4,6 +4,6 @@ def load_allstock_KRX():
     krx_url = 'https://kind.krx.co.kr/corpgeneral/corpList.do?method=download&searchType=13'
     stk_data = pd.read_html(krx_url, header=0, encoding="euc-kr")[0]
     stk_data = stk_data[['회사명', '종목코드']]
-    stk_data = stk_data.rename(columns={'회사명': 'Name', '종목코드': 'Code'})
+    stk_data = stk_data.rename(columns={'회사명': 'isinCd', '종목코드': 'itmsNm'})
     stk_data['Code'] = stk_data['Code'].apply(lambda x: str(x).zfill(6))
     return stk_data.to_dict(orient='records')
